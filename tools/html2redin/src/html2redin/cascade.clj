feca837dd893ad1ddb2b315fc66199a7fdd3701d
Sören Stöhrmann <soeren.stoehrmann@gmail.com>
2026-08-20 06:04:19 +0200

    feat(html2redin): cascade resolution with inheritance and shorthands

--- tools/html2redin/src/html2redin/cascade.clj
+++ tools/html2redin/src/html2redin/cascade.clj
@@ -31,3 +31,84 @@
   [{:keys [compounds combinators]} el ancestors]
   (and (compound-matches? (peek compounds) el)
        (match-from (rest (rseq compounds)) (seq (rseq combinators)) (vec ancestors))))
+
+(defn- four-sides [raw]
+  (let [parts (str/split (str/trim raw) #"\s+")
+        [t r b l] (case (count parts)
+                    1 (let [[a] parts] [a a a a])
+                    2 (let [[a b] parts] [a b a b])
+                    3 (let [[a b c] parts] [a b c b])
+                    (take 4 parts))]
+    [t r b l]))
+
+(defn expand-decl
+  "Expand one [prop raw important?] into longhand decls."
+  [[prop raw imp]]
+  (case prop
+    (:margin :padding)
+    (let [[t r b l] (four-sides raw)
+          base (name prop)]
+      [[(keyword (str base "-top")) t imp] [(keyword (str base "-right")) r imp]
+       [(keyword (str base "-bottom")) b imp] [(keyword (str base "-left")) l imp]])
+    :border
+    (let [tokens (str/split (str/trim raw) #"\s+")]
+      (vec (concat (when-let [w (some #(when (v/parse-length %) %) tokens)]
+                     [[:border-width w imp]])
+                   (when-let [c (some #(when (v/parse-color %) %) tokens)]
+                     [[:border-color c imp]]))))
+    :background
+    (if-let [c (some #(when (v/parse-color %) %) (str/split (str/trim raw) #"\s+"))]
+      [[:background-color c imp]] [])
+    :font
+    (let [tokens (str/split (str/trim raw) #"[\s,]+")
+          size (some #(when (v/parse-length %) %) tokens)
+          weight (some #(when (re-matches #"bold|[1-9]00" %) %) tokens)
+          family (last tokens)]
+      (vec (concat (when size [[:font-size size imp]])
+                   (when weight [[:font-weight weight imp]])
+                   (when (and family (not (v/parse-length family))) [[:font-family family imp]]))))
+    :gap [[:gap (first (str/split (str/trim raw) #"\s+")) imp]]
+    :overflow [[:overflow-y raw imp] [:overflow-x raw imp]]
+    ;; default: already a longhand
+    [[prop raw imp]]))
+
+(def inherited-props #{:color :font-size :font-family :font-weight :line-height :text-align})
+
+(defn- winning-decls
+  "Matched rules -> {prop raw}, honoring !important, specificity, order."
+  [matched]
+  (let [entries (for [r matched
+                      [prop raw imp] (mapcat expand-decl (:decls r))]
+                  {:prop prop :raw raw
+                   :key [(if imp 1 0) (:specificity r) (:order r)]})]
+    (reduce (fn [m {:keys [prop raw key]}]
+              (if (or (not (contains? m prop)) (>= (compare key (get-in m [prop :key])) 0))
+                (assoc m prop {:raw raw :key key}) m))
+            {} entries)))
+
+(defn- style-map [matched]
+  (into {} (map (fn [[k v]] [k (:raw v)]) (winning-decls matched))))
+
+(defn resolve-tree
+  "Annotate every element with :style/:own-style/:class-style/:variants."
+  [tree rules]
+  (letfn [(walk [el ancestors inherited]
+            (if-not (map? el)
+              el
+              (let [base (filter #(and (nil? (:pseudo %)) (matches? % el ancestors)) rules)
+                    own (style-map base)
+                    class-rules (filter #(seq (:classes (peek (:compounds %)))) base)
+                    class-style (style-map class-rules)
+                    variants (into {}
+                               (for [ps [:hover :focus :active]
+                                     :let [vr (filter #(and (= ps (:pseudo %))
+                                                            (matches? % el ancestors)) rules)]
+                                     :when (seq vr)]
+                                 [ps (style-map vr)]))
+                    style (merge (select-keys inherited inherited-props) own)
+                    el' (assoc el :style style :own-style own
+                               :class-style class-style :variants variants)]
+                (assoc el' :children
+                       (mapv #(walk % (conj (vec ancestors) el) style)
+                             (:children el))))))]
+    {:tree (walk tree [] {}) :warnings []}))
--- tools/html2redin/test/html2redin/cascade_test.clj
+++ tools/html2redin/test/html2redin/cascade_test.clj
@@ -18,3 +18,38 @@
     (is (cas/matches? (rule ".row > span") c [gp p]))
     (is (not (cas/matches? (rule "section > span") c [gp p])))
     (is (not (cas/matches? (rule ".nope span") c [gp p])))))
+
+(deftest shorthand-expansion
+  (is (= [[:margin-top "8px" false] [:margin-right "4px" false]
+          [:margin-bottom "8px" false] [:margin-left "4px" false]]
+         (cas/expand-decl [:margin "8px 4px" false])))
+  (is (= [[:padding-top "1px" false] [:padding-right "2px" false]
+          [:padding-bottom "3px" false] [:padding-left "4px" false]]
+         (cas/expand-decl [:padding "1px 2px 3px 4px" false])))
+  (is (= #{[:border-width "1px" false] [:border-color "#fff" false]}
+         (set (cas/expand-decl [:border "1px solid #fff" false]))))
+  (is (= [[:background-color "#2e3440" false]]
+         (cas/expand-decl [:background "#2e3440 url(x.png)" false]))))
+
+(deftest cascade-resolution
+  (let [css "div { color: red } .a { color: blue } #x { color: green } .a:hover { color: black }"
+        rules (:rules (html2redin.css/parse "t" css))
+        tree {:tag :root :attrs {} :line 1
+              :children [{:tag :div :attrs {"class" "a"} :line 1
+                          :children [{:tag :span :attrs {} :children ["hi"] :line 1}]}
+                         {:tag :div :attrs {"class" "a" "id" "x"} :children [] :line 2}]}
+        styled (:tree (cas/resolve-tree tree rules))
+        [d1 d2] (:children styled)]
+    (is (= "blue" (get-in d1 [:style :color])))          ; class beats type
+    (is (= "green" (get-in d2 [:style :color])))         ; id beats class
+    (is (= "blue" (get-in (first (:children d1)) [:style :color]))) ; inherited
+    (is (nil? (get-in (first (:children d1)) [:own-style :color]))) ; not own
+    (is (= "black" (get-in d1 [:variants :hover :color])))
+    (is (= "blue" (get-in d1 [:class-style :color])))))
+
+(deftest important-wins
+  (let [css ".a { color: red !important } #x { color: green }"
+        rules (:rules (html2redin.css/parse "t" css))
+        tree {:tag :root :attrs {} :children [{:tag :div :attrs {"class" "a" "id" "x"} :children [] :line 1}] :line 1}
+        styled (:tree (cas/resolve-tree tree rules))]
+    (is (= "red" (get-in (first (:children styled)) [:style :color])))))
